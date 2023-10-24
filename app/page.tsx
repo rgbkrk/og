57d3b00f8f5413e30a8eecf85597bd73ea236aa5
Kyle Kelley <rgbkrk@gmail.com>
2023-10-24 08:34:49 -0700

    clean up styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,16 @@ import { OGInput } from "@/components/oginput";
 
 export default function Home() {
   return (
-    <main className="">
-      <OGInput />
+    <main className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-zinc-900">
+      <div className="w-full max-w-md">
+        <h1 className="text-3xl font-bold text-zinc-600 dark:text-zinc-300 py-8">
+          Emoji Embeddings
+          <span className="text-zinc-400 dark:text-zinc-700 text-xl px-4">
+            (Top Two)
+          </span>
+        </h1>
+        <OGInput />
+      </div>
     </main>
   );
 }
